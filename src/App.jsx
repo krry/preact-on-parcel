d4eba2541d423bbd8e3c14166364419d7c20e6c6
Kerry Snyder <krry@me.com>
2019-04-15 22:33:43 -0500

    there can be only one... commit. it's that easy.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { h, Component } from "preact";
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Preactionary Measures!</h1>
+      </div>
+    )
+  }
+}
